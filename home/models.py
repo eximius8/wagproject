f2ed89e2e16dde00f11c6f99766b905996300984
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-07-04 17:33:46 +0300

    Урок 5

--- home/models.py
+++ home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.core.fields import RichTextField
 
 
@@ -19,11 +20,20 @@ class HomePage(Page):
         null=True,
     )
 
+    bg_image = models.ForeignKey(
+        'wagtailimages.Image',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
     # поля для ввода данных в интерефейсе администратора
 
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
         FieldPanel('rtfbody'),
+        ImageChooserPanel('bg_image'),
     ]
 
     # promote_panels = []
