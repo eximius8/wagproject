deb718421751aeae13444482975d32d967e2dbe5
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-07-04 14:42:58 +0300

    Урок 2

--- home/models.py
+++ home/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    
+    # поля в базе данных
+    subtitle = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    # поля для ввода данных в интерефейсе администратора
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle')
+    ]
+
+    # promote_panels = []
+    # settings_panels = []
