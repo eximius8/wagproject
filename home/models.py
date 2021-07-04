8924add362b197537aed803618a2e6c900b96bc7
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-07-04 17:16:33 +0300

    Урок 4

--- home/models.py
+++ home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
 
 
 class HomePage(Page):
@@ -13,11 +14,16 @@ class HomePage(Page):
         null=True,
         verbose_name="Подзаголовок"
     )
+    rtfbody = RichTextField(
+        blank=True,
+        null=True,
+    )
 
     # поля для ввода данных в интерефейсе администратора
 
     content_panels = Page.content_panels + [
-        FieldPanel('subtitle')
+        FieldPanel('subtitle'),
+        FieldPanel('rtfbody'),
     ]
 
     # promote_panels = []
